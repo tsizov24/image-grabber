ea20885582ef3e313ea5da01bc3559b0175b8b00
Timur Sizov <tsizov24@gmail.com>
2020-08-04 17:44:12 +0500

    Changed file saving method

--- main.go
+++ main.go
@@ -3,24 +3,29 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-		"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
 	"io"
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	url string
 )
 
 func main() {
-	url := getURL()
-	fmt.Println(url)
+	getURL()
 	geziyor.NewGeziyor(&geziyor.Options{
 		StartURLs: []string{url},
 		ParseFunc: parse,
 	}).Start()
 }
 
-func getURL() (url string) {
+func getURL() {
 	fmt.Println("Enter the web-page link: ")
 	_, err := fmt.Scanf("%s\n", &url)
 	if err != nil {
@@ -38,12 +43,12 @@ func getURL() (url string) {
 }
 
 func parse(g *geziyor.Geziyor, r *client.Response)  {
+	os.Mkdir("images", 0755)
 	num1, num2 := 0, 0
 	r.HTMLDoc.Find("img").Each(func(i int, s *goquery.Selection) {
 		for _, v := range s.Nodes[0].Attr {
 			if v.Key == "src" {
-				fmt.Println(v.Val)
-				err := downloadImage(v.Val)
+				err := downloadImage(v.Val, num1)
 				if err != nil {
 					num2++
 				} else {
@@ -56,13 +61,17 @@ func parse(g *geziyor.Geziyor, r *client.Response)  {
 	fmt.Printf("Images downloaded: %d\n", num1)
 }
 
-func downloadImage(path string) error {
+func downloadImage(path string, n int) error {
+	re := regexp.MustCompile(`^http`)
+	if !re.MatchString(path) {
+		path = url + path
+	}
 	resp, err := http.Get(path)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	out, err := os.Create(path[len(path)-10:])
+	out, err := os.Create("images/" + strconv.FormatInt(int64(n + 1), 10) + path[strings.LastIndex(path, ".") : ])
 	if err != nil {
 		return err
 	}
